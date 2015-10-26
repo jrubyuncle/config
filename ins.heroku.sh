28aba830e8d53e9a5880054696424aafac8ff831
jrubyuncle <jrubyuncle@gmail.com>
2015-10-26 15:39:20 +0800

    add ins.heroku.sh

---
+++ ins.heroku.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo ""
+echo "to install heroku toolbelt"
+echo "ref: https://toolbelt.heroku.com/debian"
+
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+echo
+echo "[$0]"
